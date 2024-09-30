7113cfc8f47e9bdd8bd2c6155950b227dfd01f3e
Jalal-Elhajouji <jalalelhajouji@gmail.com>
2024-09-30 02:30:48 +0100

    NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className='flex items-center justify-between py-3 font-medium'>
+
+            <a href='/' className='flex items-baseline'>
+                <h2 className='text-zinc-600 text-2xl font-bold italic'>CLOTHER</h2>
+                <p className='text-zinc-600'>&reg;</p>
+            </a>
+
+            <ul className='hidden sm:flex gap-3 text-sm text-zinc-600'>
+
+                <NavLink to='/' className='flex flex-col items-center gap-1' >
+                    <p>HOME</p>
+                    <hr className='w-1/2 border-none h-[1.5px] bg-zinc-600 hidden' />
+                </NavLink>
+                <NavLink to='/collection' className='flex flex-col items-center gap-1' >
+                    <p>COLLECTION</p>
+                    <hr className='w-1/2 border-none h-[1.5px] bg-zinc-600 hidden' />
+                </NavLink>
+                <NavLink to='/about' className='flex flex-col items-center gap-1' >
+                    <p>ABOUT</p>
+                    <hr className='w-1/2 border-none h-[1.5px] bg-zinc-600 hidden' />
+                </NavLink>
+                <NavLink to='/contact' className='flex flex-col items-center gap-1' >
+                    <p>CONTACT</p>
+                    <hr className='w-1/2 border-none h-[1.5px] bg-zinc-600 hidden' />
+                </NavLink>
+            </ul>
+
+            <div className='flex items-center gap-6'>
+                <img src={assets.search_icon} className='w-5 cursor-pointer' alt="img" />
+                <Link to='/cart' className='relative'>
+                    <img src={assets.cart_icon} className='w-5 cursor-pointer' alt="img" />
+                    <span className='absolute -top-1 -right-2 w-4 h-4 bg-zinc-400 text-xs text-white flex items-center justify-center rounded-full'>3</span>
+                </Link>
+                <div className='group relative'>
+                    <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile" />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-200 text-zinc-600 rounded-md'>
+                            <p className='cursor-pointer hover:text-black'>My Profile</p>
+                            <p className='cursor-pointer hover:text-black'>Orders</p>
+                            <p className='cursor-pointer hover:text-black'>Global</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
